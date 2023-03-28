0ef61254040f7e19930d37f27db000a64096b666
sutongsheng <sutongsheng@soundai.com>
2023-03-28 17:36:25 +0800

    初始提交

--- app/src/main/java/com/jetpack/first/MainActivity.kt
+++ app/src/main/java/com/jetpack/first/MainActivity.kt
@@ -2,7 +2,11 @@ package com.jetpack.first
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+/**
+ * @author SuTs
+ * @create 17:36 2023/3/28
+ * @Describe
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
