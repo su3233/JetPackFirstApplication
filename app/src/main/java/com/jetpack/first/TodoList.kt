61cd17a3d363a7bc51bd38d1f194d2f9a5014a61
sutongsheng <sutongsheng@soundai.com>
2023-03-31 16:57:39 +0800

    todolist

---
+++ app/src/main/java/com/jetpack/first/TodoList.kt
@@ -0,0 +1,19 @@
+package com.jetpack.first
+
+/**
+ * @project: JetPackFirstApplication
+ * @author: tongSheng.su on 2023/3/31 16:55
+ * @Description：
+ */
+// TODO:
+
+/**
+ * todo
+ *
+ * Kotlin协程的简单用法（GlobalScope、lifecycleScope、viewModelScope）
+ * https://blog.csdn.net/Goals1989/article/details/126964335
+
+Kotlin学习笔记（四）——get/set、主次构造函数、初始化
+https://blog.csdn.net/mm731296278/article/details/125968471
+
+ */
